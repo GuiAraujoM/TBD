e7e967300af783ab6775ea4897e05de52940f878
GuiAraujoM <garaujomedeiros.14@gmail.com>
2023-03-26 16:50:08 -0300

    Commit 2

--- Trabalho1/src/main/java/apresentacao/Main.java
+++ Trabalho1/src/main/java/apresentacao/Main.java
@@ -5,23 +5,253 @@
 
 package apresentacao;
 
+import util.JPAUtil;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import negocio.*;
+import persistencia.*;
 
-public class Main {
-    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MeuPU");    
+public class Main {  
 
     public static void main(String[] args) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();        
-
+        /*EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
         Funcionario f = new Funcionario();
         f.setNome("Betito ");
         em.persist(f);
         em.getTransaction().commit();
-        em.close();
+        em.close();*/
+        /*GenericoDAO dao = new GenericoDAO<Funcionario>();
+        Funcionario f1 = new Funcionario();
+        f1.setNome("Betito ");
+        dao.salvar(f1);
+        Funcionario f2 = (Funcionario)dao.consultarPorId(Funcionario.class, 1);
+        System.out.println(f2.getNome());*/
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Insira a operação que deseja realizar");
+            System.out.println("(1 - Criar, 2 - Editar, 3 - Apagar, 4 - Consultar)");
+            int operacao = sc.nextInt();
+
+            System.out.println("Insira a entidade da operação que deseja realizar");
+            System.out.println("(1 - Funcionario, 2 - Paciente, 3 - Atendimento, 4 - Setor)");
+            int entidade = sc.nextInt();
+
+            menu(operacao, entidade);
+        }
+
+    }
+
+    public static void menu(int operacao, int entidade){
+        Scanner sc = new Scanner(System.in);
+        String stringInput;
+
+        FuncionarioDAO funcDAO = new FuncionarioDAO();
+        PacienteDAO pacDAO = new PacienteDAO();
+        AtendimentoDAO atendDAO = new AtendimentoDAO();
+        SetorDAO setorDAO = new SetorDAO();
+        
+        if(operacao == 1){ //Criar
+            if(entidade == 1){ //Funcionario
+                
+                Funcionario func = Funcionario.criarFuncionario();               
+
+                try {
+                    func = funcDAO.salvar(func);
+                    System.out.println("Funcionario criado");
+                    System.out.println(func.toString());
+                } catch (Exception e) {
+                    throw e;
+                }
+
+            }else if (entidade == 2) { //Paciente
+
+                Paciente pac = Paciente.criarPaciente();
+
+                try {
+                    pac = pacDAO.salvar(pac);                    
+                    System.out.println("Paciente criado");
+                    System.out.println(pac.toString());
+                } catch (Exception e) {
+                    throw e;
+                }
+
+            }else if (entidade == 3) { //Atendimento
+
+                Atendimento atend = Atendimento.criarAtendimento();
+
+                try {
+                    atend = atendDAO.salvar(atend);
+                    System.out.println("Atendimento criado");
+                    System.out.println(atend.toString());
+                } catch (Exception e) {
+                    throw e;
+                }
+
+            }else if(entidade == 4){ //Setor
+                
+                Setor setor = Setor.criaSetor();                
+
+                try {
+                    setor = setorDAO.salvar(setor);
+                    System.out.println("Setor criado");
+                    System.out.println(setor.toString());
+                } catch (Exception e) {
+                    throw e;
+                }
+            }
+        }else if(operacao == 2){ //Editar
+            if(entidade == 1){      
+
+                System.out.println("Insira o cpf do funcionário que deseja editar");
+                stringInput = sc.nextLine();
+                Funcionario func = funcDAO.consultarPorCpf(stringInput);
+
+                if(func != null){
+                    int idFunc = func.getId();
+                    System.out.println("Funcionário encontrado!");
+                    System.out.println(func.toString());
+                    
+                    func = Funcionario.criarFuncionario();
+                    func.setId(idFunc);                    
+                    funcDAO.salvar(func);
+
+                    System.out.println("Funcionario atualizado");
+                    System.out.println(func.toString());
+                }else{
+                    System.out.println("Funcionário não encontrado, tente novamente");
+                } 
+
+            }else if(entidade == 2){ //Paciente
+
+                System.out.println("Insira o cpf do paciente que deseja editar");
+                stringInput = sc.nextLine();
+                Paciente pac = pacDAO.consultarPorCpf(stringInput);
+
+                if (pac != null) {
+                    int idPac = pac.getId();
+                    System.out.println("Paciente encontrado!");
+                    System.out.println(pac.toString());
+
+                    pac = Paciente.criarPaciente();
+                    pac.setId(idPac);
+                    pacDAO.salvar(pac);
+
+                    System.out.println("Paciente atualizado");
+                    System.out.println(pac.toString());
+                } else {
+                    System.out.println("Paciente não encontrado, tente novamente");
+                }
+
+            }else if(entidade == 3){ //Atendimento
+
+                System.out.println("Insira o id do atendimento que deseja editar");
+                stringInput = sc.nextLine();
+                Atendimento atend = atendDAO.consultarPorId(Atendimento.class, Integer.parseInt(stringInput));
+
+                if (atend != null) {
+                    int idAtend = atend.getId();
+                    System.out.println("Atendimento encontrado!");
+                    System.out.println(atend.toString());
+
+                    atend = Atendimento.criarAtendimento();
+                    atend.setId(idAtend);
+                    atendDAO.salvar(atend);
+
+                    System.out.println("Atendimento atualizado");
+                    System.out.println(atend.toString());
+                } else {
+                    System.out.println("Atendimento não encontrado, tente novamente");
+                }
+
+            }else if(entidade == 4){ //Setor
+
+                System.out.println("Insira o nome do setor que deseja editar");
+                stringInput = sc.nextLine();
+                Setor setor = setorDAO.consultarPorNome(stringInput);
+
+                if (setor != null) {
+                    int idSetor = setor.getId();
+                    System.out.println("Setor encontrado!");
+                    System.out.println(setor.toString());
+
+                    setor = Setor.criaSetor();
+                    setor.setId(idSetor);
+                    setorDAO.salvar(setor);
+
+                    System.out.println("Setor atualizado");
+                    System.out.println(setor.toString());
+                } else {
+                    System.out.println("Setor não encontrado, tente novamente");
+                }
+
+            }
+        }else if(operacao == 3){ //Deletar
+            if(entidade == 1){
+
+                System.out.println("Insira o cpf do funcionário que deseja deletar");
+                stringInput = sc.nextLine();                
+                try{
+                    funcDAO.remover(Funcionario.class, funcDAO.consultarPorCpf(stringInput).getId());
+                }catch(Exception e){
+                    System.out.println("Erro ao deletar: " + e.getMessage());
+                }
+                    
+            }else if(entidade == 2){
+
+                System.out.println("Insira o cpf do paciente que deseja deletar");
+                stringInput = sc.nextLine();
+                try {
+                    pacDAO.remover(Paciente.class, pacDAO.consultarPorCpf(stringInput).getId());
+                } catch (Exception e) {
+                    System.out.println("Erro ao deletar: " + e.getMessage());
+                }
+
+            }else if(entidade == 3){
+
+                System.out.println("Insira o id do atendimento que deseja deletar");
+                stringInput = sc.nextLine();
+                try {
+                    atendDAO.remover(Atendimento.class, atendDAO.consultarPorId(Atendimento.class, Integer.parseInt(stringInput)).getId());
+                } catch (Exception e) {
+                    System.out.println("Erro ao deletar: " + e.getMessage());
+                }
+
+            }else if(entidade == 4){
+
+                System.out.println("Insira o nome do setor que deseja deletar");
+                stringInput = sc.nextLine();
+                try {
+                    setorDAO.remover(Setor.class, setorDAO.consultarPorNome(stringInput).getId());
+                } catch (Exception e) {
+                    System.out.println("Erro ao deletar: " + e.getMessage());
+                }
+
+            }
+        }else if(operacao == 4){
+            if(entidade == 1){
+                System.out.println("Insira o cpf do funcionário que deseja consultar");
+                stringInput = sc.nextLine();
+                funcDAO.consultarPorCpf(stringInput).toString();
+            }else if(entidade == 2){
+                System.out.println("Insira o cpf do paciente que deseja consultar");
+                stringInput = sc.nextLine();
+                pacDAO.consultarPorCpf(stringInput).toString();
+            }else if(entidade == 3){
+                System.out.println("Insira o id do atendimento que deseja consultar");
+                stringInput = sc.nextLine();
+                atendDAO.consultarPorId(Atendimento.class, Integer.parseInt(stringInput)).toString();
+            }else if(entidade == 4){
+                System.out.println("Insira o nome do setor que deseja consultar");
+                stringInput = sc.nextLine();
+                setorDAO.consultarPorNome(stringInput).toString();
+            }
+        }
     }
 }
